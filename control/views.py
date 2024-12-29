9bd9fad84e3c7fdd397481894dbe4c2418f75944
angelo-francisco <ics20080729@gmail.com>
2024-12-29 23:05:52 +0100

    new file: signup.html

--- control/views.py
+++ control/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request): 
+    ctx = {}
+    template_name = "control/signup.html"
+
+
+    if request.method == "POST":
+        ...
+    
+    return render(request, template_name, ctx)
