448097e7f985a21f5d869a5df6c25c45b8e2dcb2
angelo-francisco <ics20080729@gmail.com>
2024-12-30 16:36:22 +0100

    new model: reader

--- control/models.py
+++ control/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.utils.text import slugify
+
+class Reader(models.Model):
+    GRADE_CHOICES = (
+        ('10', 'Décima'),
+        ('11', 'Décima Primeira'),
+        ('12', 'Décima Segunda'),
+    )     
+
+    GROUP_CHOICES = (
+        ('A', 'Turma A'),
+        ('B', 'Turma B'),
+        ('C', 'Turma C'),
+        ('D', 'Turma D'),
+    )   
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="reader")
+    grade = models.CharField("Classe", max_length=3, choices=GRADE_CHOICES)
+    group = models.CharField("Turma", max_length=3, choices=GROUP_CHOICES)
+    process_number = models.CharField("Número de processo", max_length=5, unique=True)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.first_name + self.user.last_name)
+            
+        super().save(*args, **kwargs)
