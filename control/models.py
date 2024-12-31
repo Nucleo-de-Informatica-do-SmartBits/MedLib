851428bac0a2d5876d22eb513d629ef3d45e0f54
zaya <samuelzaya41@gmail.com>
2024-12-31 20:49:29 +0000

    Alter choices in model Reader to correct way

--- control/models.py
+++ control/models.py
@@ -5,28 +5,28 @@ from django.utils.text import slugify
 
 # Create your models here.
 class Reader(models.Model):
-    GRADE_CHOICES: dict = (
+    GRADE_CHOICES: list[tuple[str, str]] = [
         ('10', 'Décima'),
         ('11', 'Décima Primeira'),
         ('12', 'Décima Segunda'),
-    )     
-    GROUP_CHOICES: dict = {
+    ]
+    GROUP_CHOICES: list[tuple[str, str]] = [
         ('A', 'Turma A'),
         ('B', 'Turma B'),
         ('C', 'Turma C'),
         ('D', 'Turma D'),
-    }
-    COURSE_CHOICES: dict = {
-        "Informática": 'Informática',
-        "Eletrónica": 'Eletrónica'
-    }
+    ]
+    COURSE_CHOICES: list[tuple[str, str]] = [
+        ("Informática", 'Informática'),
+        ("Eletrónica", 'Eletrónica')
+    ]
     user: User = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="reader")
     process_number: int = models.IntegerField(unique=True)
     grade: str = models.CharField("Classe", max_length=3, choices=GRADE_CHOICES)
     course: str = models.CharField("Curso",max_length=30, choices=COURSE_CHOICES)
     group: str = models.CharField("Turma", max_length=3, choices=GROUP_CHOICES)
     photo: str = models.FileField("Perfil",upload_to='profiles-photo/')
-    slug = models.SlugField()
+    slug: str = models.SlugField()
 
     def save(self, *args, **kwargs):
         if not self.slug:
