a014862116d4e36a5df4f40e233ceab7e9888b27
angelo-francisco <ics20080729@gmail.com>
2025-01-02 17:14:48 +0100

    update to use forms(django forms)

---
+++ control/forms.py
@@ -0,0 +1,289 @@
+import unicodedata
+from secrets import compare_digest
+
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+from .models import Reader
+
+
+class ReaderCreationForm(forms.Form):
+    firstname = forms.CharField(
+        label="Primeiro Nomee",
+        min_length=3,
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "formbold-form-input", "placeholder": "Primeiro nome"}
+        ),
+    )
+
+    lastname = forms.CharField(
+        label="Sobrenome",
+        min_length=3,
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "formbold-form-input", "placeholder": "Sobrenome"}
+        ),
+    )
+
+    process_number = forms.CharField(
+        label="Número de processo",
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "formbold-form-input",
+                "placeholder": "Número de processo, exemplo: 1234",
+                "id": "num-process",
+            }
+        ),
+    )
+
+    course = forms.ChoiceField(
+        label="Curso",
+        choices=[
+            ("Informática", "Informática"),
+            ("Eletrónica", "Eletrónica"),
+        ],
+        widget=forms.Select(
+            attrs={
+                "class": "formbold-form-input",
+                "id": "occupation",
+                "required": "",
+            }
+        ),
+    )
+
+    grade = forms.ChoiceField(
+        label="Classe",
+        choices=[
+            ("10", "Décima"),
+            ("11", "Décima Primeira"),
+            ("12", "Décima Segunda"),
+            ("13", "Décima Terceira"),
+        ],
+        widget=forms.Select(
+            attrs={
+                "class": "formbold-form-input",
+                "id": "occupation",
+                "required": "",
+            }
+        ),
+    )
+
+    group = forms.ChoiceField(
+        label="Turma",
+        choices=[
+            ("A", "Turma A"),
+            ("B", "Turma B"),
+            ("C", "Turma C"),
+            ("D", "Turma D"),
+        ],
+        widget=forms.RadioSelect(attrs={"class": "turma-option"}),
+        required=True,
+    )
+
+    password = forms.CharField(
+        label="Palavra-passe",
+        required=True,
+        min_length=8,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "formbold-form-input",
+                "minlength": 8,
+                "required": "",
+                "placeholder": "Palavra-passe",
+            }
+        ),
+    )
+
+    confirm_password = forms.CharField(
+        label="Confirmar Palavra-passe",
+        required=True,
+        min_length=8,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "formbold-form-input",
+                "minlength": 8,
+                "required": "",
+                "placeholder": "Confirmar Palavra-passe",
+            }
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["course"].choices.insert(0, ("", "Selecione o seu curso"))
+        self.fields["grade"].choices.insert(0, ("", "Selecione a sua classe"))
+
+    def clean_process_number(self):
+        process_number = self.cleaned_data.get("process_number", "")
+
+        if (
+            not process_number.isdigit()
+            or Reader.objects.filter(process_number=process_number).exists()
+        ):
+            raise forms.ValidationError("Número de processo inválido ou já registrado")
+
+        return process_number
+
+    def clean_course(self):
+        course = self.cleaned_data.get("course", "")
+
+        if course not in ("Informática", "Electrónica"):
+            raise forms.ValidationError("Este curso não está disponível!")
+
+        return course
+
+    def clean_grade(self):
+        grade = self.cleaned_data.get("grade", "")
+
+        if grade not in ("10", "11", "12", "13"):
+            raise forms.ValidationError("Classe inválida!")
+
+        return grade
+
+    def clean_group(self):
+        group = self.cleaned_data.get("group", "")
+
+        if group not in ("A", "B", "C", "D"):
+            raise forms.ValidationError("Turma inválida")
+
+        return group
+
+    def clean_firstname(self):
+        firstname = self.cleaned_data.get("firstname", "")
+
+        if not firstname.strip():
+            raise forms.ValidationError("Preencha o campo, por favor!")
+
+        return firstname
+
+    def clean_lastname(self):
+        lastname = self.cleaned_data.get("lastname", "")
+
+        if not lastname.strip():
+            raise forms.ValidationError("Preencha o campo, por favor!")
+
+        return lastname
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password", "").strip()
+
+        if not password:
+            raise forms.ValidationError("Por favor, digite a palavra-passe!")
+
+        if len(password) < 8:
+            raise forms.ValidationError("A palavra-passe deve ter pelo menos 8 caracteres!")
+
+        return password
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get("password", "")
+        confirm_password = self.cleaned_data.get("confirm_password", "")
+
+        if not confirm_password:
+            raise forms.ValidationError("Por favor, confirme a palavra-passe!")
+
+        if not compare_digest(password, confirm_password):
+            raise forms.ValidationError("As palavras-passe não coincidem!")
+
+        return confirm_password
+
+    def create_username(self, firstname, lastname):
+        _base_username = unicodedata.normalize(
+            "NFKD", f"{firstname}-{lastname}".lower()
+        )
+        base_username = "".join(
+            [c for c in _base_username if not unicodedata.combining(c)]
+        )
+
+        count = User.objects.filter(username__startswith=base_username).count()
+        return f"{base_username}-{count + 1}" if count else base_username
+
+    def save(self):
+        data = super().clean()
+
+        user = User.objects.create_user(
+            username=self.create_username(
+                firstname=data.get("firstname"), lastname=data.get("lastname")
+            ),
+            first_name=data.get("firstname"),
+            last_name=data.get("lastname"),
+            password=data.get("password"),
+        )
+
+        reader = Reader.objects.create(
+            user=user,
+            process_number=data.get("process_number"),
+            course=data.get("course"),
+            grade=data.get("grade"),
+            group=data.get("group"),
+        )
+
+        reader.save()
+
+
+class ReaderAuthenticationForm(forms.Form):
+    process_number = forms.CharField(
+        label="Número de processo",
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "formbold-form-input mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
+                "placeholder": "Número de processo",
+                "id": "num-process",
+            }
+        ),
+    )
+
+    password = forms.CharField(
+        label="Palavra-passe",
+        required=True,
+        min_length=8,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "formbold-form-input mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
+                "minlength": 8,
+                "placeholder": "Palavra-passe",
+                "id": "password"
+            }
+        ),
+    )
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user = None
+
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        process_number = cleaned_data.get("process_number", "").strip()
+        password = cleaned_data.get("password", "").strip()
+
+        if not process_number or not password:
+            raise forms.ValidationError("Por favor, preencha todos os campos!")
+        
+        if (not process_number.isdigit()) or int(process_number) < 0:
+            raise forms.ValidationError("Número de processo inválido.")
+
+        reader = Reader.objects.filter(process_number=process_number).first()
+
+        if not reader:
+            self.add_error("process_number", "Número de processo inválido")
+        else:
+            self.user = authenticate(
+                self.request, username=reader.user.username, password=password
+            )
+
+            if not self.user:
+                raise forms.ValidationError("Número de processo ou Palavra-passe incorrectos")
+
+        return cleaned_data
+
+    def get_user(self):
+        return self.user
+        
+        
--- control/views.py
+++ control/views.py
@@ -1,13 +1,11 @@
-import unicodedata
-from secrets import compare_digest
-
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout as auth_logout
+from django.contrib.auth import login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .models import Reader, User
+from .forms import ReaderAuthenticationForm, ReaderCreationForm
 
 
 def just_not_authenticated_user(func):
@@ -15,126 +13,53 @@ def just_not_authenticated_user(func):
         if request.user.is_authenticated:
             messages.info(request, "Você já está logado!")
             return redirect(reverse("home"))
-        
+
         return func(request, *args, **kwargs)
 
     return wrapper
 
 
-def validate_form(request):
-    firstname = request.POST.get("firstname")
-    lastname = request.POST.get("lastname")
-    process_number = request.POST.get("num-process")
-    course = request.POST.get("course")
-    grade = request.POST.get("grade")
-    group = request.POST.get("group")
-    password = request.POST.get("password")
-    confirm_password = request.POST.get("confirm-password")
-
-    if not compare_digest(password, confirm_password):
-        messages.warning(request, "As palavras-passe não coincidem!")
-        return False
-
-    elif len(firstname.strip()) == 0 or len(lastname.strip()) == 0:
-        messages.error(request, "Preencha todos os campos, por favor!")
-        return False
-
-    elif (
-        process_number.isdigit()
-        or Reader.objects.filter(process_number=process_number).exists()
-    ):
-        messages.error(request, "Número de processo inválido ou já registrado")
-        return False
-
-    elif course not in ("Informática", "Electrónica"):
-        messages.error(request, "Este curso não está disponível!")
-        return False
-
-    elif grade not in ("10", "11", "12", "13"):
-        messages.error(request, "Classe inválida!")
-        return False
-
-    elif group not in ("A", "B", "C", "D"):
-        messages.error(request, "Turma inválida")
-        return False
-
-    return True
-
-
-def create_username(firstname, lastname):
-    _base_username = unicodedata.normalize("NFKD", f"{firstname}-{lastname}".lower())
-    base_username = "".join([c for c in _base_username if not unicodedata.combining(c)])
-
-    count = User.objects.filter(username__startswith=base_username).count()
-    return f"{base_username}-{count + 1}" if count else base_username
-
-
 @just_not_authenticated_user
 def signup(request):
+    ctx = {}
     template_name = "control/signup.html"
 
     if request.method == "POST":
-        firstname = request.POST.get("firstname")
-        lastname = request.POST.get("lastname")
-        process_number = request.POST.get("num-process")
-        course = request.POST.get("course")
-        grade = request.POST.get("grade")
-        group = request.POST.get("group")
-        password = request.POST.get("password")
+        form = ReaderCreationForm(request.POST)
 
-        if validate_form(request):
-            user = User.objects.create_user(
-                username=create_username(firstname=firstname, lastname=lastname),
-                first_name=firstname,
-                last_name=lastname,
-                password=password,
-            )
+        if form.is_valid():
+            form.save()
 
-            reader = Reader.objects.create(
-                user=user,
-                process_number=process_number,
-                grade=grade,
-                course=course,
-                group=group,
-            )
-            reader.save()
+            messages.success(request, "Dados registrados com sucesso!")
+            return redirect(reverse("signin"))
+    else:
+        form = ReaderCreationForm()
 
-            messages.success(request, "Os seus dados foram registrados!")
-            return redirect("signin")
-
-        return redirect(reverse("signup"))
-    return render(request, template_name)
+    ctx["form"] = form
+    return render(request, template_name, ctx)
 
 
 @just_not_authenticated_user
 def signin(request):
+    ctx = {}
     template_name = "control/signin.html"
 
     if request.method == "POST":
-        process_number = request.POST.get("num-process")
-        password = request.POST.get("password")
+        form = ReaderAuthenticationForm(data=request.POST, request=request)
 
-        if not process_number.strip() or not password.strip():
-            messages.error(request, "Por favor, preencha todos os campos1")
-            return redirect(reverse("signin"))
+        if form.is_valid():
+            user = form.get_user()
 
-        if not process_number.isdigit() or int(process_number) < 0:
-            messages.error(request, "Número de processo inválido.")
-            return redirect(reverse("signin"))
+            if user is not None:
+                login(request, user)
 
-        reader = get_object_or_404(Reader, process_number=process_number)
-        user = authenticate(request, username=reader.user.username, password=password)
+                messages.success(request, "Sessão iniciada com sucesso!")
+                return redirect(reverse("home"))
+    else:
+        form = ReaderAuthenticationForm()
 
-        if user is not None:
-            login(request, user)
-
-            messages.success(request, "Sessão iniciada com sucesso!")
-            return redirect(reverse("home"))
-
-        messages.warning(request, "Username ou Palavra-passe incorrectos!")
-        return redirect(reverse("signin"))
-
-    return render(request, template_name)
+    ctx["form"] = form
+    return render(request, template_name, ctx)
 
 
 @login_required
