ce98a42b885776d440ded8d6bcbafebfd0fe8158
angelo-francisco <ics20080729@gmail.com>
2025-01-10 17:13:06 +0100

    linking each html to a js file

---
+++ assets/js/control/signin.js
@@ -0,0 +1,22 @@
+const processNumber = document.querySelector("#num-process")
+processNumber.addEventListener("input", (event) => {
+    const value = event.data
+
+    if (!Number(value) && Number(value) !== 0) {
+        processNumber.value = processNumber.value.replace(value, "")
+    }
+
+    if (processNumber.value.length > 4) {
+        processNumber.value = processNumber.value.slice(0, 4)
+    }
+
+    if (value === " ") {
+        processNumber.value = processNumber.value.replace(value, "")
+    }
+
+    if (processNumber.value.length > 0) {
+        processNumber.style.fontSize = '25px'
+    } else {
+        processNumber.style.fontSize = '15px'
+    }
+})
---
+++ assets/js/library/home.js
@@ -0,0 +1,33 @@
+let currentIndex = 0;
+const images = document.querySelectorAll("#image-slider img");
+
+const frasesArray = [
+    "\"A leitura é a chave para abrir portas para o mundo do conhecimento e da imaginação.\"",
+    "\"A leitura nos dá asas para voar nas mais diversas imaginações.\"",
+    "\"Cada livro é uma viagem esperando para ser vivida\"",
+]
+
+const totalImages = images.length;
+
+function changeImage() {
+    let text = document.querySelector('#text')
+
+    currentIndex = (currentIndex + 1) % totalImages;
+    console.log(currentIndex)
+
+    const offset = -currentIndex * 100;
+    document.getElementById("image-slider").style.transform = `translateX(${offset}%)`;
+
+    text.innerText = frasesArray[currentIndex]
+}
+
+setInterval(changeImage, 3000);
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "flex";
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
