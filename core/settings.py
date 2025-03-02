924cdd6d109f0ab69b08a3fb0572966c1a530523
angelo-francisco <ics20080729@gmail.com>
2025-03-02 20:39:53 +0100

    ready for deployv7

--- core/settings.py
+++ core/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(os.getenv("DATABASE_URL")),
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL")),
 }
 
 
