d500c2d5219d27d38f962866157e0a0523132d5c
angelo-francisco <ics20080729@gmail.com>
2025-03-02 18:33:52 +0100

    *new app: humanize from django added to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # "crispy_forms",
     # "crispy_tailwind",
     "django_browser_reload",
