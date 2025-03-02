0fb942f7a0456f867cba2100fe562a45823923c6
angelo-francisco <ics20080729@gmail.com>
2025-03-02 18:13:58 +0100

    *new: add model Comment on registers

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Book, Category, Publisher, Sugestion
+from .models import Author, Book, Category, Publisher, Sugestion, Comment
 
 # Register your models here.
-admin.site.register([Author, Book, Category, Publisher, Sugestion])
+admin.site.register([Author, Book, Category, Publisher, Sugestion, Comment])
