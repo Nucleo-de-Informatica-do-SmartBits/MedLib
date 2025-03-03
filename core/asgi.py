dae67e5704f02e310ca3e7a07d8e8048949cb768
angelo-francisco <ics20080729@gmail.com>
2025-03-03 19:55:38 +0100

    ready for deplov19

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,15 @@
-import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-
-from library.routing import urlpatterns as websocket_urlpatterns
+import os, django #noqa
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter #noqa
+from channels.security.websocket import AllowedHostsOriginValidator #noqa
+from channels.auth import AuthMiddlewareStack #noqa
+from django.core.asgi import get_asgi_application #noqa
+
+from library.routing import urlpatterns as websocket_urlpatterns #noqa
+
 
 dj_application = get_asgi_application()
 
